ccf86abe94a23c6c1d1a59933bd079220844c833
Anton <antonbj3@msn.com>
2026-09-05 23:00:24 +0200

    motbevis -> enhet: test_skriptbeteende_maste_upptackas_oavsett_hur_typen_skrivs[nyckelordsargument]

--- tests/enhet/test_skrivgrind.py
+++ tests/enhet/test_skrivgrind.py
@@ -315,6 +315,7 @@ SKRIPTBETEENDEN_MB = [
     ("t = VC_SCRIPT\nc.createBehaviour(t, 'x')", "typen via en variabel"),
     ("c.createBehaviour(vcConst.VC_SCRIPT, 'x')", "typen via ett attribut"),
     ("c.createBehaviour(*[VC_SCRIPT, 'x'])", "uppackade argument"),
+    ("c.createBehaviour(**{'type': VC_SCRIPT, 'name': 'x'})", "nyckelordsargument"),
 ]
 
 
--- tests/motbevis/test_skrivgrind_motbevis.py
+++ tests/motbevis/test_skrivgrind_motbevis.py
@@ -48,7 +48,6 @@ def test_en_skrivning_skriven_som_dunderanrop_maste_ocksa_fastna(kod, skal):
 # ---- 2. skriptbeteendegrinden är fail-open ------------------------------
 
 SKRIPTBETEENDEN = [
-    ("c.createBehaviour(**{'type': VC_SCRIPT, 'name': 'x'})", "nyckelordsargument"),
     ("setattr(b, 'Script', kod)", "tilldelning till .Script via setattr"),
 ]
 
