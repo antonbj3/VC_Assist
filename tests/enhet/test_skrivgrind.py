b307d12322d5665c82f4d752f1aea4f05763f48f
Anton <antonbj3@msn.com>
2026-09-05 23:00:00 +0200

    motbevis -> enhet: test_skriptbeteende_maste_upptackas_oavsett_hur_typen_skrivs[uppackade argument]

--- tests/enhet/test_skrivgrind.py
+++ tests/enhet/test_skrivgrind.py
@@ -314,6 +314,7 @@ def test_samma_operation_far_inte_bero_pa_hur_den_stavas():
 SKRIPTBETEENDEN_MB = [
     ("t = VC_SCRIPT\nc.createBehaviour(t, 'x')", "typen via en variabel"),
     ("c.createBehaviour(vcConst.VC_SCRIPT, 'x')", "typen via ett attribut"),
+    ("c.createBehaviour(*[VC_SCRIPT, 'x'])", "uppackade argument"),
 ]
 
 
